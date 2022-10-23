bf6b9c89ff2b68d8d41a65dad62562f4e9b249af
Luna <iret0014@dtonic.io>
2022-10-23 10:16:56 +0900

    fix StatCalculate and DamageCalculate

--- ProjectM/Entity.cpp
+++ ProjectM/Entity.cpp
@@ -5,34 +5,43 @@ void Entity::StatCalculate()
 {
 	//
 	//디펜스는 아이템 디펜스
-	_attack = (float)_Stat._str * 1.15;
-	_hp = (int)_Stat._str * 5;
-	_attackSpeed = (float)_Stat._dex * 1.15;
-	_dropRate = (float)_Stat._luk * 0.15;
-	_dodgeRate = (float)_Stat._dex * 0.15;
-	_critRate = (float)_Stat._luk * 2.25;
-	_critDamage = (float)_Stat._luk * 1.5;
+	_attack = (float)_Stat._str * 2 + _Stat._dex * 0.5 + _Stat._luk * 0.5;
+	_hp = (int)_Stat._str * 10;
+	_attackSpeed = (float)_Stat._dex * 0.05;
+	_dropRate = (float)_Stat._luk * 0.1;
+	_dodgeRate = (float)_Stat._dex * 0.1;
+	_critRate = (float)_Stat._luk * 0.7;
+	_critDamage = (float)_Stat._luk * 0.2;
+	_skillDamage = (float)_Stat._int * 1;
+	_mp = (float)_Stat._int * 7; 
 	
 }
 
 void Entity::Attack(shared_ptr<Entity> enemy)
 {
-	double damage = (_attack * 100) / (enemy->_defence + 100);
-
+	double damage = _attack + (_attack * DamageCalcualte());
+	
 }
 void Entity::Defence(shared_ptr<Entity> enemy, int enemySelect)
 {
+
 	// 선택이 공격였다면 데미지 감소식 or 반격
 	// 선택이 방어었다면 아무일도 일어나지 않고 리턴
+	
+	switch (enemySelect) {
+	case ATTACK:
+		
+	case DEFENCE:
+		return;
+	}
 }
 
 int  Entity::DamageCalcualte()
 {
-	bool isCritical = true;
 
-	if (isCritical)
+	if ((rand() % 100) < _critRate)
 	{
-		(rand() % 100) < _critRate;
+		return _critDamage;
 		
 	}
 	return 0;
