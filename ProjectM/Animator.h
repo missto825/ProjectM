a01a6c790e8ee52989ea747cfb32aac7c130e8ce
missto825 <suhwan825@gmail.com>
2022-11-16 12:45:43 +0900

    파일정리 및 에니메이터 정의

--- ProjectM/Animator.h
+++ ProjectM/Animator.h
@@ -1,9 +1,24 @@
 #pragma once
 #include "pch.h"
 #include "SpriteSheet.h"
+struct Trigger
+{
+	
+};
+struct Animation
+{
+	int num;
+	SpriteSheet ani;
+	
+};
 class Animator
 {
 public:
+	Animator(int num);
+public:
+	vector<Animation> animation;
+
+	void ChangeAnimation(int num);
 	
 };
 
